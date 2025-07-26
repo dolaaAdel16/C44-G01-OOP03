7cad39cb9d2f8ef2fc6b0a3f5d47476b1ab07948
Ahmed <aa3452000@gmail.com>
2025-07-26 20:21:35 +0300

    V01

---
+++ DemoOOP03/Interface Ex01/MyType.cs
@@ -0,0 +1,20 @@
+﻿using DemoOOP03.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOOP03.Interface_Ex01
+{
+    internal class MyType : IMyType
+    {
+        public double salary{ get; set; }
+
+        public void MyFun()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+}
