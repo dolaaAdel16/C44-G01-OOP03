0353b52f91134891a52ec10d6e6e3ced3a86fa3f
Ahmed <aa3452000@gmail.com>
2025-07-27 17:16:00 +0300

    .

--- DemoOOP03/Program.cs
+++ DemoOOP03/Program.cs
@@ -4,7 +4,82 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region V01
+            //Console.WriteLine("V01");  
+
+            //MyType myType = new MyType();
+            //myType.salary = 12000; 
+            //Console.WriteLine(myType.salary);
+            //myType.MyFun();
+
+            //myType.print(); 
+
+            //IMyType mytype1 = new IMyType();
+            //myType1.myfun();
+
+            //IMyType myType;
+
+            //myType = new MyType();
+
+            //myType.salary = 12000;
+            //Console.WriteLine(myType.salary);
+            //myType.MyFun();
+            //myType.print(); 
+            #endregion
+
+            #region V02
+            //Console.WriteLine("V02")
+            //SeriesByTwo seriesbytwo = new SeriesByTwo();
+            //SeriesByThree seriesbythree = new SeriesByThree();
+            //SeriesByFour seriesbyfour = new SeriesByFour(); 
+
+            //PrintTenNumbers(seriesbytwo);
+
+            //int[] Numbers = { 4, 8, 6, 1, 0, 7, 2, 5 }; 
+
+            //Array.Sort(Numbers);
+
+            //foreach (int item in Numbers)
+            //{ 
+            //    Console.Write($"{item}" + " ");    
+            //}
+
+
+            //Employee[] employees = new Employee[3]
+            //{
+            //    new Employee(){ Id = 1, Name = "Ahemd", Age = 22, Salary = 12000 } ,
+            //    new Employee(){ Id = 2, Name = "Omar", Age = 24, Salary = 15000 } ,
+            //    new Employee(){ Id = 1, Name = "Mostafa", Age = 27, Salary = 20000 }
+            //};
+
+            //Array.Sort(employees);
+
+            //foreach (Employee item in employees)
+            //{
+            //    Console.WriteLine(item);   
+            //} 
+            #endregion
+
+            #region V03
+            //Console.WriteLine("V03");
+
+            //Car car = new Car(); 
+
+            //car.Speed = 100;
+            //car.Forward(); 
+
+            //Airplane airplane = new Airplane();
+
+            //IMoveable moveable = airplane;
+            //moveable.Forward(); 
+
+            //IFlyabe flyabe = airplane;
+            //flyabe.Forward();    
+            #endregion
+
+            J
+
+
         }
     }
 }
